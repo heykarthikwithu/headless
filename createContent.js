315a949e8fd4ba3cb99fe1982104a2e976eae59d
heykarthikwithu <heykarthikwithu@gmail.com>
2016-12-25 12:32:27 +0530

    Create content with Js, auth need to be cross checked.

---
+++ createContent.js
@@ -0,0 +1,45 @@
+
+function getCsrfToken(callback) {
+  jQuery
+    //.get(Drupal.url('http://127.0.0.1/d82/rest/session/token'))
+    .get('http://127.0.0.1/d82/rest/session/token')
+    .done(function (data) {
+      var csrfToken = data;
+      callback(csrfToken);
+    });
+}
+
+function postNode(csrfToken, node) {
+  jQuery.ajax({
+    url: 'http://127.0.0.1/d82/entity/node?_format=hal_json',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/hal+json',
+      'X-CSRF-Token': csrfToken
+    },
+    data: JSON.stringify(node),
+    success: function (node) {
+      console.log(node);
+    }
+  });
+}
+
+var newNode = {
+  _links: {
+    type: {
+      href: 'http://127.0.0.1/d82/rest/type/node/article'
+    }
+  },
+  type: {
+    target_id: 'article'
+  },
+  title: {
+    value: 'Example node title'
+  }
+};
+
+function createContent() {
+  getCsrfToken(function (csrfToken) {
+    postNode(csrfToken, newNode);
+  });
+}
